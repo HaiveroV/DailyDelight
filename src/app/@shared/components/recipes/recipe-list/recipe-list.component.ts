57f9dde14a1d12e76b77f32db588ba61f1225f70
Dimitar Hayverov <mhaiverov@gmail.com>
2019-06-05 17:08:56 +0300

    Made Recipe structure component

---
+++ src/app/@shared/components/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../../../models/recipe/recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
