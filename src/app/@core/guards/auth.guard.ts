90957e9d2b12036b2e33e229775d7a04ade262bd
Dimitar Hayverov <mhaiverov@gmail.com>
2019-06-07 16:42:06 +0300

    future authguard implementation

--- src/app/@core/guards/auth.guard.ts
+++ src/app/@core/guards/auth.guard.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AlertService } from '../services/alert.service'
 import { Observable } from 'rxjs';
+import { tap, map, take } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard //implements CanActivate 
-{
-  constructor(private authService: AuthService){}
-  // canActivate(
-    // next: ActivatedRouteSnapshot,
-    // state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+export class AuthGuard {
 
-    //    return this.authService.isLoggedIn();
+  not
 
-//}
+  constructor(
+    private authService: AuthService,
+    public alertService: AlertService,
+    private router: Router
+  ) { }
+
+ 
+
+
 }
+
